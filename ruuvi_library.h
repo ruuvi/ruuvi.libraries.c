bd79fd7b11b29c39979dddd8d5dda885aab00772
Otso Jousimaa <otso@ojousima.net>
2019-10-22 14:17:54 +0300

    Add internal error code

--- ruuvi_library.h
+++ ruuvi_library.h
@@ -14,12 +14,13 @@
 
 
 #define RUUVI_LIBRARY_SUCCESS 0                 //!< Success
-#define RUUVI_LIBRARY_ERROR_NO_MEM      (1<<0)  //!< Not enough memory for operation
-#define RUUVI_LIBRARY_ERROR_CONCURRENCY (1<<1)  //!< Failed to acquire mutex
-#define RUUVI_LIBRARY_ERROR_NULL        (1<<2)  //!< Unexpected NULL pointer
-#define RUUVI_LIBRARY_ERROR_NO_DATA     (1<<3)  //!< Unxpected end of data
-#define RUUVI_LIBRARY_ERROR_SELFTEST    (1<<4)  //!< Self-test noticed invalid output
-#define RUUVI_LIBRARY_ERROR_DATA_LENGTH (1<<5)  //!< Data length is invalid for any reason
+#define RUUVI_LIBRARY_ERROR_INTERNAL    (1<<0)  //!< Unknown error, some failed assumption. 
+#define RUUVI_LIBRARY_ERROR_NO_MEM      (1<<1)  //!< Not enough memory for operation
+#define RUUVI_LIBRARY_ERROR_CONCURRENCY (1<<2)  //!< Failed to acquire mutex
+#define RUUVI_LIBRARY_ERROR_NULL        (1<<3)  //!< Unexpected NULL pointer
+#define RUUVI_LIBRARY_ERROR_NO_DATA     (1<<4)  //!< Unxpected end of data
+#define RUUVI_LIBRARY_ERROR_SELFTEST    (1<<5)  //!< Self-test noticed invalid output
+#define RUUVI_LIBRARY_ERROR_DATA_LENGTH (1<<6)  //!< Data length is invalid for any reason
 #define RUUVI_LIBRARY_ERROR_FATAL       (1<<31) //!< Unrecoverable error, restart program
 
 #define RUUVI_LIBRARY_I32_INVALID       INT32_MIN
