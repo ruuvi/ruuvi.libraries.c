2fb6ab4e2370ab26ca3cb45a606aad1434700e61
Otso Jousimaa <otso@ojousima.net>
2020-07-06 14:35:54 +0300

    Add GCov plugin to ceedling

---
+++ test/test_ruuvi_library_rms.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+
+#include "ruuvi_library_rms.h"
+
+void setUp (void)
+{
+}
+
+void tearDown (void)
+{
+}
+
+void test_ruuvi_library_rms_NeedToImplement (void)
+{
+    TEST_IGNORE_MESSAGE ("Need to Implement ruuvi_library_rms");
+}
